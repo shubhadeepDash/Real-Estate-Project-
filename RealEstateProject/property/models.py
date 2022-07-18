71769dab7161af37e3b8842a10f59c22da7654ca
shubhadeepDash <108353165+shubhadeepDash@users.noreply.github.com>
2022-07-18 13:21:50 +0530

    Add files via upload

---
+++ RealEstateProject/property/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Property(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    longitude = models.DecimalField(max_digits=11, decimal_places=7)
+    latitude = models.DecimalField(max_digits=11, decimal_places=7)
+    length = models.DecimalField(max_digits=20, decimal_places=5)
+    breadth = models.DecimalField(max_digits=20, decimal_places=5)
+    purpose = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class PropertyPictures(models.Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    image = models.ImageField(default="default.jpg",upload_to='property_pics')
---
+++ RealEstateProject/user/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    pincode = models.IntegerField(null=True, blank=True)
+    phone_number = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name
