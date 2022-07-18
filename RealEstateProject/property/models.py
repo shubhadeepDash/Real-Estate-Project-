0008c661e64a6ec133ea07cbb2ce056da6091576
shubhadeepDash <108353165+shubhadeepDash@users.noreply.github.com>
2022-07-18 14:03:44 +0530

    comment check

--- RealEstateProject/property/models.py
+++ RealEstateProject/property/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here... comment
 class Property(models.Model):
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=60)
