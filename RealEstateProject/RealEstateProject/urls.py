86c639a6e98b9b935af8d91c62e31535da9ed0a5
satyaG117 <goswamisatya2003@gmail.com>
2022-07-22 17:35:44 +0530

    left some files in last commit

--- RealEstateProject/RealEstateProject/urls.py
+++ RealEstateProject/RealEstateProject/urls.py
@@ -27,9 +27,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
-    path('sell/', views.AddressView.as_view(template_name='maps/map.html'), name='map'),
+    # path('sell/', views.AddressView.as_view(template_name='maps/map.html'), name='map'),
+    path('sell/', views.sellProperty, name='sell'),
     path('register/', user_views.register,name='register'),
     path('profile/', user_views.profile, name='profile'),
+    # path('propertypics/<int:fk>/',views.propertyPics,name='property-pics')
 ]
 
 if settings.DEBUG:
--- RealEstateProject/property/views.py
+++ RealEstateProject/property/views.py
@@ -1,6 +1,8 @@
+from msilib.schema import Property
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PropertySellForm
+from django.db.models import Q
 
 def index(request):
     return render(request, 'property/home.html')
@@ -15,3 +17,16 @@ def sellProperty(request):
             return redirect('property-home')
     context = {"form": form}
     return render(request, "property/sell.html", context)
+
+def results(request):
+    q = request.GET.get('q')
+    properties = Property.objects.filter(
+        Q(city__icontains = q) |
+        Q(state__icontains = q) |
+        Q(country__icontains = q) |
+        Q(pincode = int(q))
+    )
+
+    context = {'properties' : properties}
+
+    return render(request,'property/results.html',context)
